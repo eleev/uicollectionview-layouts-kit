10a6ac4f389c8d2fb30570e19554d5b6eddf0f85
Astemir Eleev <astemireleev@gmail.com>
2018-04-16 17:48:30 +0300

    Implemented ComicsCell

---
+++ pinterest/ComicsCell.swift
@@ -0,0 +1,32 @@
+//
+//  ComicCell.swift
+//  pinterest
+//
+//  Created by Astemir Eleev on 16/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+class ComicsCell: UICollectionViewCell {
+
+    // MARK: - Statics
+    
+    static let reusalbeId = "comics-cell"
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var coverImage: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    // MARK: - Overrides
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        containerView.layer.cornerRadius = 10
+        containerView.layer.masksToBounds = true
+    }
+        
+}
