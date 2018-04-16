b16bf6ee04a5a75c68e7480e9895695f647304fa
Astemir Eleev <astemireleev@gmail.com>
2018-04-16 17:47:23 +0300

    Implemented DesignableView class

---
+++ collection-flow-layout-ios/View/DesignableView.swift
@@ -0,0 +1,55 @@
+//
+//  DesignableView.swift
+//  collection-flow-layout-ios
+//
+//  Created by Astemir Eleev on 16/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class DesignableView: UIView {
+    
+    public var contentView : UIView?
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetup()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        xibSetup()
+    }
+    
+    func xibSetup() {
+        contentView = loadViewFromNib()
+        
+        // use bounds not frame or it'll be offset
+        contentView!.frame = bounds
+        
+        // Make the view stretch with containing view
+        contentView!.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleHeight]
+        
+        // Adding custom subview on top of our view (over any custom drawing > see note below)
+        addSubview(contentView!)
+        
+    }
+    
+    func loadViewFromNib() -> UIView! {
+        
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        
+        return view
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        config()
+    }
+    
+    public func config() { /* Empty implementation, intended to be used to child classes */}
+}
