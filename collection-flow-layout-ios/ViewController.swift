6d6e2e60ef4d3d7515fc5a79accfa4249f888ffa
Astemir Eleev <astemireleev@gmail.com>
2018-04-16 09:22:53 +0300

    Initial Commit

---
+++ collection-flow-layout-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  collection-flow-layout-ios
+//
+//  Created by Astemir Eleev on 16/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
