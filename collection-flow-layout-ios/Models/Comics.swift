3de7beb34fe79e15348f1276e574a6610b44cb7b
Astemir Eleev <astemireleev@gmail.com>
2018-04-16 17:46:44 +0300

    Created Comics struct

---
+++ collection-flow-layout-ios/Models/Comics.swift
@@ -0,0 +1,37 @@
+//
+//  Comics.swift
+//  pinterest-collection-flow-layout-ios
+//
+//  Created by Astemir Eleev on 16/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+struct Comics {
+    
+    // MARK: - Properties
+    
+    var image: UIImage
+    var caption: String
+    var comment: String
+    
+    // MARK: - Initializers
+    
+    init(image: UIImage, caption: String, comment: String) {
+        self.image = image
+        self.caption = caption
+        self.comment = comment
+    }
+    
+    init?(dictionary: [String : String]) {
+        guard let image = dictionary["Cover"], let cover = UIImage(named: image), let caption = dictionary["Caption"], let comment = dictionary["Comment"] else {
+            debugPrint(#function + " one of the data properties from ComicData.plist could not be unwrapped or is nil")
+            return nil
+        }
+        self.init(image: cover, caption: caption, comment: comment)
+    }
+    
+   
+    
+}
