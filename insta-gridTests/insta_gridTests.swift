a4aaae20c9976af1f6193218771c6f44e53c3ef0
Astemir Eleev <astemireleev@gmail.com>
2018-04-18 18:46:54 +0300

    Created new target for insta-grid

---
+++ insta-gridTests/insta_gridTests.swift
@@ -0,0 +1,36 @@
+//
+//  insta_gridTests.swift
+//  insta-gridTests
+//
+//  Created by Astemir Eleev on 18/04/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import XCTest
+@testable import insta_grid
+
+class insta_gridTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
