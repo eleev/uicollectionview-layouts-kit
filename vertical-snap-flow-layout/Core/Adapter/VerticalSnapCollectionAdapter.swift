6fe7452e85d53a6b82196a10ccceabc4bb764ee2
Astemir Eleev <astemireleev@gmail.com>
2018-05-22 11:35:45 +0300

    Created VerticalSnapCollectionAdapter struct

---
+++ vertical-snap-flow-layout/Core/Adapter/VerticalSnapCollectionAdapter.swift
@@ -0,0 +1,18 @@
+//
+//  VerticalSnapCollectionAdapter.swift
+//  vertical-snap-flow-layout
+//
+//  Created by Astemir Eleev on 22/05/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+struct  VerticalSnapCollectionAdapter {
+    
+    // MARK: - Methods
+    
+    func configure(cell: VerticalSnapCollectionViewCell, forDisplaying comics: Comics) {
+        cell.imageName = comics.imageName
+    }
+}
